05c262c70ccf7c4b9b3e278e1abbf7210a637b41
Damjan <damjan.roskar.s@gmail.com>
2020-05-22 22:18:30 +0200

    keep surgery output

--- surgery.bat
+++ surgery.bat
@@ -2,3 +2,4 @@
 docker cp .\keyboards\ergodox_ez\keymaps\surgeon-general\keymap.c qmk:qmk_firmware/keyboards/ergodox_ez/keymaps/surgeon-general/
 docker start -i qmk
 docker cp qmk:qmk_firmware/ergodox_ez_surgeon-general.hex ./..
+PAUSE
